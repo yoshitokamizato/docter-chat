570821967c21f70e5b37ef165a168d285fd4c9ed
terasaka187 <billionsystem187@gmail.com>
2019-10-19 13:05:00 +0900

    model_clam

---
+++ db/migrate/20191019035826_create_doctors.rb
@@ -0,0 +1,11 @@
+class CreateDoctors < ActiveRecord::Migration[6.0]
+  def change
+    create_table :doctors do |t|
+      t.string :name
+      t.integer :age
+      t.references :revue
+      t.references :hospital
+      t.timestamps
+    end
+  end
+end
