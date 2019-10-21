b9c7b572023bfba45acac44ccaef18768b219d97
奥村彰良 <aonk13965@gmail.com>
2019-10-21 10:30:33 +0900

    add gem devise

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_20_090240) do
+ActiveRecord::Schema.define(version: 2019_10_21_010023) do
 
   create_table "medical_examinations", force: :cascade do |t|
     t.integer "weight"
@@ -29,6 +29,13 @@ ActiveRecord::Schema.define(version: 2019_10_20_090240) do
     t.string "sex"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
---
+++ vendor/bundle/ruby/2.6.0/gems/ffi-1.11.1/ext/ffi_c/libffi-x86_64-darwin18/include/ffitarget.h
@@ -0,0 +1,1 @@
+/Users/aki/Desktop/docter-chat/vendor/bundle/ruby/2.6.0/gems/ffi-1.11.1/ext/ffi_c/libffi/src/x86/ffitarget.h
